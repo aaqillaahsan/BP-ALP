1f787ea38297c607f0be81f461e74da6c68f98bf
aaqillaahsan <aaqillaahsan@student.ciputra.ac.id>
2025-12-24 20:55:53 +0700

    Update Item.java

--- MysteryMansion/src/Item.java
+++ MysteryMansion/src/Item.java
@@ -1,3 +1,23 @@
 public class Item {
     private String name;
+    private String description;
+    private boolean isClue;
+
+    public Item (String name, String description, boolean isClue){
+        this.name = name;
+        this.description = description;
+        this.isClue = isClue;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDesc(){
+        return name + ": " + description;
+    }
+
+    public boolean isClue(){
+        return isClue;
+    }
 }
