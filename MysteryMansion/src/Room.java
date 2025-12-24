f3adff6a7b914382ff751e2ee788dbee9ad5d7ed
aaqillaahsan <aaqillaahsan@student.ciputra.ac.id>
2025-12-24 20:56:57 +0700

    Implement Room class with properties and methods

--- MysteryMansion/src/Room.java
+++ MysteryMansion/src/Room.java
@@ -1,3 +1,50 @@
-public class Room {
 
+import java.util.ArrayList;
+
+public class Room {
+    private String name;
+    private String description;
+    private ArrayList<Item> items;
+    private ArrayList<NPC> npcs;
+    private ArrayList<Room> connections;
+
+    public Room (String name, String description){
+        this.name = name;
+        this.description = description;
+        items = new ArrayList<>();
+        npcs = new ArrayList<>();
+        connections = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String Describe(){
+        return name + "\n" + description;
+    }
+
+    public ArrayList<Room> getConnections(){
+        return connections;
+    }
+
+    public ArrayList<Item> getItems(){
+        return items;
+    }
+
+    public ArrayList<NPC> getNPCS(){
+        return npcs;
+    }
+    
+    public void connectRoom(Room room){
+        connections.add(room);
+    }
+
+    public void addItems(Item item){
+        items.add(item);
+    }
+
+    public void addNPCS(NPC npc){
+        npcs.add(npc);
+    }
 }
