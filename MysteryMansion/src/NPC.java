74881f2d851dfadd634af15cb32fe36c9bab6674
aaqillaahsan <aaqillaahsan@student.ciputra.ac.id>
2025-12-24 20:56:43 +0700

    Update NPC.java

--- MysteryMansion/src/NPC.java
+++ MysteryMansion/src/NPC.java
@@ -1,3 +1,25 @@
 public class NPC {
+    private String name;
+    private String dialogue;
+    private boolean isCulprit;
+    private boolean giveClue;
 
+    public NPC (String name, String dialogue, boolean isCulprit, boolean giveClue){
+        this.name = name;
+        this.dialogue = dialogue;
+        this.isCulprit = isCulprit;
+        this.giveClue = giveClue;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+    public String Talk(){
+        return dialogue;
+    }
+
+    public boolean getCulprit(){
+        return isCulprit;
+    }
 }
