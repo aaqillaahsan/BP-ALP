b99d841baa8628ff0099b892e681cf180e221441
aaqillaahsan <aaqillaahsan@student.ciputra.ac.id>
2026-01-01 05:52:12 +0000

    fix file

--- CrackedWallClue.java
+++
@@ -1,8 +0,0 @@
-public class CrackedWallClue extends EnviroClue{
-
-    public CrackedWallClue(){
-        name = "Slightly Cracked Wall";
-        desc = "A foul smell permeates out of it. Should ask Groundskeeper.";
-        isReal = true;
-    }
-}
--- NoDef.java
+++
@@ -1,8 +0,0 @@
-public class NoDef extends EnviroClue{
-
-    public NoDef(){
-        name = "No Defensive Wound";
-        desc = "There are no signs of resistance. The attacker might be a relative.";
-        isReal = false;
-    }
-}
