d0048fd286e35faa612de4141b9f0f9e5066cada
aaqillaahsan <aaqillaahsan@student.ciputra.ac.id>
2026-01-01 05:44:28 +0000

    Revert "Delete ArthurBody.java"
    This reverts commit 25de402a9e07d858da9c1004cb8dace4010ad137.

---
+++ ArthurBody.java
@@ -0,0 +1,18 @@
+public class ArthurBody extends Interactable{
+
+    public ArthurBody(){
+        name = "Arthur's Body";
+
+        conditionEClues.add(new ConditionEClue(new ArthurCon(), 4, null));
+    }
+
+    @Override
+    protected String hasInteracted(){
+        return "You've had enough of his unsightly body. You feel repulsed to check again.";
+    }
+
+    @Override
+    protected String needsomething(Player player){
+        return "You need something.";
+    }
+}
