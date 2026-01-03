31b92f7002d6dd48f5b01309ca57c422537c57b3
aaqillaahsan <aaqillaahsan@student.ciputra.ac.id>
2026-01-03 07:24:40 +0000

    Revert "Add files via upload"
    This reverts commit 08fc93a5f9e447069b975dc674d39415b29983c7.

--- ArthurCon.java
+++
@@ -1,8 +0,0 @@
-public class ArthurCon extends EnviroClue{
-
-    public ArthurCon(){
-        name = "Arthur's Bloodied Body";
-        desc = "The body seems to have no pulse. Slight rot detected.\nThe wounds inflicted seem to be caused by blunt object. There is no slash nor stab marks";
-        isReal = true;
-    }
-}
--- FootprintClue.java
+++
@@ -1,8 +0,0 @@
-public class FootprintClue extends EnviroClue{
-
-    public FootprintClue(){
-        name = "Footprints near basement window";
-        desc = "Footprints of quite a sizable measure. Impossible for a child.";
-        isReal = true;
-    }
-}
