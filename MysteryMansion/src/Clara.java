9ef97bd46a8ca2f2a552a6fc95748b4f83e13772
aaqillaahsan <aaqillaahsan@student.ciputra.ac.id>
2026-01-04 18:41:47 +0700

    Add files via upload

--- MysteryMansion/src/Clara.java
+++ MysteryMansion/src/Clara.java
@@ -5,14 +5,20 @@ public class Clara extends NPC{
 
         //Builds first night's dialogue
         NightDialogue n1 = new NightDialogue("Default Dialogue (night 1)");
-        n1.addDialogue("Dialogue 1 (Introductionn)");
-        n1.addDialogue("Dialogue 2 (Explains what happened)");
-        n1.addDialogue("Dialogue 3 (Mention fighting in living room");
+        n1.addDialogue("Oh… I didn’t expect anyone else wandering the kitchen this late. I’m Clara. I’ve kept this house clean for yearslong before tonight’s mess.");
+        n1.addDialogue("Mr. Arthur was in a dreadful mood after dinner. Voices were raised nothing unusual for this family, sadly.\n" + "Then suddenly… he was gone. Doors locked, tempers flared, and now we’re all prisoners in this mansion.");
+        n1.addDialogue("I heard shouting from the living room earlier. Arthur and Charles again, I think. Charles stormed off afterward red faced, gripping something in his hand. A glass, maybe… or worse.");
+        n1.addDialogue("That knife? It belongs in the kitchen, but I found it missing after dinner. Charles was helping himself to wine earlier… wouldn’t surprise me if he grabbed it in a rage.");
+        n1.addDialogue("Here take this key. It opens the cabinet beneath the counter. If we’re trapped here, we may as well uncover the truth… even if it makes people uncomfortable.");
         nightDialogue.add(n1);
 
         //Builds second night's dialogue
         NightDialogue n2 = new NightDialogue("Default Dialogue (night 2");
-        n2.addDialogue("Dialogue 1 ()");
+        n2.addDialogue("This house feels heavier tonight… like it’s holding its breath.\n" + "Arthur may have been difficult, but no one deserves to vanish like that.");
+        n2.addDialogue("Mr. Arthur was in a dreadful mood after dinner. Voices were raised nothing unusual for this family, sadly.\n" + "Then suddenly… he was gone. Doors locked, tempers flared, and now we’re all prisoners in this mansion.");
+        n2.addDialogue("I heard shouting from the living room earlier. Arthur and Charles again, I think. Charles stormed off afterward red faced, gripping something in his hand. A glass, maybe… or worse.");
+        n2.addDialogue("That knife? It belongs in the kitchen, but I found it missing after dinner. Charles was helping himself to wine earlier… wouldn’t surprise me if he grabbed it in a rage.");
+        n2.addDialogue("Here take this key. It opens the cabinet beneath the counter. If we’re trapped here, we may as well uncover the truth… even if it makes people uncomfortable.");
         nightDialogue.add(n2);
 
         //Builds third night's dialogue
