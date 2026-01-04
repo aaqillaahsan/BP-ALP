2348bd6fcb1512f824f8acbcf6edcb0f9697185c
aaqillaahsan <aaqillaahsan@student.ciputra.ac.id>
2026-01-04 19:36:37 +0700

    Add files via upload

--- MysteryMansion/src/Thomas.java
+++ MysteryMansion/src/Thomas.java
@@ -4,30 +4,30 @@ public class Thomas extends NPC{
         super("Thomas", false);
 
         //Builds first night's dialogue
-        NightDialogue n1 = new NightDialogue("Everything feels calm as the night begins.");
+        NightDialogue n1 = new NightDialogue("Default Dialogue (night 1)");
         n1.addDialogue("Dialogue 1 (Introductionn)");
         n1.addDialogue("Dialogue 2 (Explains what happened)");
         n1.addDialogue("Dialogue 3 (Mention fighting in living room");
         nightDialogue.add(n1);
 
         //Builds second night's dialogue
-        NightDialogue n2 = new NightDialogue("As the night continues, the atmosphere subtly changes.");
+        NightDialogue n2 = new NightDialogue("Default Dialogue (night 2");
         n2.addDialogue("Dialogue 1 ()");
         nightDialogue.add(n2);
 
         //Builds third night's dialogue
-        NightDialogue n3 = new NightDialogue("The atmosphere grows heavier as the night progresses.");
+        NightDialogue n3 = new NightDialogue("Default Dialogue (night 3)");
         n3.addDialogue("Dialogue 1 ()");
         nightDialogue.add(n3);
 
         //Builds fourth night's dialogue
-        NightDialogue n4 = new NightDialogue("Everything feels more intense as the night unfolds.");
+        NightDialogue n4 = new NightDialogue("Default Dialogue (night 4)");
         n4.addDialogue("Dialogue 1 ()");
         nightDialogue.add(n4);
 
         //Item Logic
-        conditionItem.add(new ConditionItem(new GardenShedKey(), 3, 0, null, null, "You might need this key for the Garden Shed"));
-        conditionItem.add(new ConditionItem(new Hammer(), 4, 0, null, "Slightly Cracked Wall", "Dialog give hammer"));
+        conditionItem.add(new ConditionItem(new GardenShedKey(), 3, 0, null, null, "Take this key to open the garden shed"));
+        conditionItem.add(new ConditionItem(new Hammer(), 4, 0, null, "Slightly Cracked Wall", "You find a cracked wall, this hammer might help you."));
         setDesc();
     }
 
