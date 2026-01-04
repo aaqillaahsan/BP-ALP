381599f0550f8100c9a0fd42bc8389ecee12c1ad
aaqillaahsan <aaqillaahsan@student.ciputra.ac.id>
2026-01-04 18:28:28 +0700

    Update Bedroom.java

--- MysteryMansion/src/Bedroom.java
+++ MysteryMansion/src/Bedroom.java
@@ -12,6 +12,6 @@ public class Bedroom extends Room{
 
     @Override
     protected String roomDesc(){
-        return "Bedroom Desc";
+        return "A room full of folder stacked everywhere yet surprisingly tidy";
     }
 }
