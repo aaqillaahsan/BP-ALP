b4447f33eff00fb03f83e1a6a597e9e37fb2ce6e
aaqillaahsan <aaqillaahsan@student.ciputra.ac.id>
2026-01-04 19:38:33 +0700

    Add files via upload

--- Edmund.java
+++ Edmund.java
@@ -4,33 +4,33 @@ public class Edmund extends NPC{
         super("Edmund", false);
 
         //Builds first night's dialogue
-        NightDialogue n1 = new NightDialogue("It feels like an ordinary night so far.");
+        NightDialogue n1 = new NightDialogue("Default Dialogue (night 1)");
         n1.addDialogue("Dialogue 1 (Introductionn)");
         n1.addDialogue("Dialogue 2 (Explains what happened)");
         n1.addDialogue("Dialogue 3 (Mention fighting in living room");
         nightDialogue.add(n1);
 
         //Builds second night's dialogue
-        NightDialogue n2 = new NightDialogue("The house seems quieter than usual tonight.");
+        NightDialogue n2 = new NightDialogue("Default Dialogue (night 2");
         n2.addDialogue("Dialogue 1 ()");
         nightDialogue.add(n2);
 
         //Builds third night's dialogue
-        NightDialogue n3 = new NightDialogue("Something feels different, even if it cannot be named.");
+        NightDialogue n3 = new NightDialogue("Default Dialogue (night 3)");
         n3.addDialogue("Dialogue 1 ()");
         nightDialogue.add(n3);
 
         //Builds fourth night's dialogue
-        NightDialogue n4 = new NightDialogue("There is a feeling that this night matters.");
+        NightDialogue n4 = new NightDialogue("Default Dialogue (night 4)");
         n4.addDialogue("Dialogue 1 ()");
         nightDialogue.add(n4);
 
         //Clue Logic
-        conditionClues.add(new ConditionClue(new DialogClue("Locked Mansion",getName(), "Dialog mansion locked", true), 1, 0, null,null));
+        conditionClues.add(new ConditionClue(new DialogClue("Locked Mansion",getName(), "Ever since Arthur went missing, the mansion has been kept locked", true), 1, 0, null,null));
 
         //Item Logic
-        conditionItem.add(new ConditionItem(new GardenKey(), 3, 0, null, null, "Dialog give key"));
-        conditionItem.add(new ConditionItem(new BasementKey(), 3, 0, null, null, "Dialog give key"));
+        conditionItem.add(new ConditionItem(new GardenKey(), 3, 0, null, null, "Take this key to open the garden"));
+        conditionItem.add(new ConditionItem(new BasementKey(), 3, 0, null, null, "Take this key to open the basement"));
         setDesc();
     }
 
