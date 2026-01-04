b147719c37ddfbb71bcd79bfef7cb40ac7034a5c
aaqillaahsan <aaqillaahsan@student.ciputra.ac.id>
2026-01-04 19:54:54 +0700

    Revise dialogue for Thomas in night scenes
    Updated Thomas's dialogue for the third and fourth nights to provide more context and depth to the character's interactions.

--- MysteryMansion/src/Thomas.java
+++ MysteryMansion/src/Thomas.java
@@ -17,12 +17,18 @@ public class Thomas extends NPC{
 
         //Builds third night's dialogue
         NightDialogue n3 = new NightDialogue("The atmosphere grows heavier as the night progresses.");
-        n3.addDialogue("Dialogue 1 ()");
+        n3.addDialogue("Name’s Thomas. I take care of the grounds and the outer parts of the estate.\n" + "Didn’t expect to be questioned like this.");
+        n3.addDialogue("I was outside most of the evening.\n" + "Gardens don’t tend themselves, even at night.");
+        n3.addDialogue("Yeah, I’ve got tools. Shovels, crowbars… comes with the job.\n" + "Doesn’t mean I used them on a person.");
+        n3.addDialogue("You might need this key for the garden shed.\n" + "Everything I own is in there.");
         nightDialogue.add(n3);
 
         //Builds fourth night's dialogue
         NightDialogue n4 = new NightDialogue("Everything feels more intense as the night unfolds.");
-        n4.addDialogue("Dialogue 1 ()");
+        n4.addDialogue("Things have turned serious.\n" + "If there’s something hidden, it won’t stay that way much longer.");
+        n4.addDialogue("I noticed footprints near the basement window.\n" + "Too careful to be someone panicking.");
+        n4.addDialogue("This hammer might help.\n" + "There’s a wall in the basement that always looked… wrong.");
+        n4.addDialogue("I work with my hands, sure.\n" + "But whoever did this wasn’t acting on desperation—they had a plan.");
         nightDialogue.add(n4);
 
         //Item Logic
